6f42581d01fa94168ede8cd5e6015acaa413f8ab
DreadParadox-Dash <70744199+DreadParadox-Dash@users.noreply.github.com>
2024-09-12 16:52:41 +0800

    Migration of gradle plugins from legacy apply script syntax to plugin dsl syntax

--- lib/Screens/register_screen.dart
+++ lib/Screens/register_screen.dart
@@ -5,6 +5,52 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Scheduler')),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.all(8),
+        child: RegisterForm(),
+      ),
+    );
   }
 }
+
+//The Form to register an account
+class RegisterForm extends StatefulWidget {
+  const RegisterForm({super.key});
+
+  @override
+  State<RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final _formkey = GlobalKey<FormState>();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Email'),
+            TextFormField()
+          ],
+        ),
+    );
+  }
+}
+
